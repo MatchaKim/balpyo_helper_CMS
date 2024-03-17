3cbb73237e95f36b2f2ef1b040d6ab9f5e9168c4
MatchaKim <kimjunsik333@gmail.com>
2024-03-17 16:16:35 +0900

    Chore :: reset.css 적용 및 샘플페이지 작성

--- app.vue
+++ app.vue
@@ -1,5 +1,14 @@
 <template>
-  <div>
-    <NuxtWelcome />
+  <div style="background-color:#EB2A63; width:100%; height:100vh; display:flex; flex-direction:column; justify-content:center; align-items:center; text-align:center;">
+    <div style="color:white; font-size: 24px; font-weight: bold;">
+      <p style="font-size: 26px;">WELCOME TO 발표몇분</p>
+      <p style="padding: 10px;font-size: 12px">구름톤 준비하느라 발표 5번은 고치셨죠?</p>
+      <p style="padding: 10px;font-size: 12px">Coming Soon...</p>
+    </div>
   </div>
+  
 </template>
+
+<style>
+@import "@/assets/css/reset.css";
+</style>
